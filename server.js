bbf96379a1d0e1e40d3d46658fcde806f8a9caf4
Jinal Mistry <jinalmistry1505@gmail.com>
2023-05-08 14:14:39 +0530

    second commit

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ let client = [];
 //TCP server
 const server = net.createServer((csocket) => {
     // console.log(`New Client connection from ${csocket.remoteAddress}:${csocket.remotePort}`);
-    console.log("Connected");
+    // console.log("Connected");
     //new client socket to arr
     client.push(csocket);
     //message form client
     csocket.on('data', (data) => {
         client.forEach((socket) => {
             socket.write(data);
-            console.log(data.toString());
         })
+        console.log(data.toString());
     })
 })
 server.listen(3000, () => console.log('Server is listening on port Number 3000'));
